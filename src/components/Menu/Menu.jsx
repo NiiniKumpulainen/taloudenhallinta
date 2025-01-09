f4c961b734a5c98c141f6964b2977db6099e1e2b
Niini Kumpulainen <p22567@edu.sasky.fi>
2025-01-09 17:14:15 +0200

    lisää linkityksen Menu-komponenttiin

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,16 +2,17 @@ import styles from './Menu.module.scss'
 import { MdViewList } from 'react-icons/md'
 import { MdTimeline } from 'react-icons/md'
 import { MdSettings } from 'react-icons/md'
+import { NavLink } from 'react-router-dom'
 
 function Menu() {
 
   return (
     <div className={styles.menu}>
-            <div><MdViewList /></div>
+    <div><NavLink to=""><MdViewList /></NavLink></div>
+    <div><NavLink to="/stats"><MdTimeline /></NavLink></div>
+    <div><NavLink to="/settings"><MdSettings /></NavLink></div>
+  </div>
 
-      <div><MdTimeline /></div>
-      <div><MdSettings /></div>
-    </div>
   )
 
 }
