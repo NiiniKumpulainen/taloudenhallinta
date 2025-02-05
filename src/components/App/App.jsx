65dc0d22e6a3232ad1fef082915eef71da068c63
Niini Kumpulainen <p22567@edu.sasky.fi>
2025-02-05 11:55:16 +0200

    lisää tietojen tallennuksen localStorage-taltioon

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,16 @@
 import AppRouter from '../AppRouter'
-import testdata from './testdata.js'
+
 import { useState } from 'react'
+import useLocalStorage from '../../shared/uselocalstorage'
 
 
 
 
 function App() {
 
-  const [data, setData] = useState(testdata)
-  const [typelist, setTypelist] = useState(["Auto", "Puhelin", "Sähkö", "Vero", "Vesi"])
+  const [data, setData] = useLocalStorage('taloudenhallinta-data',[])
+  const [typelist, setTypelist] = useLocalStorage('taloudenhallinta-typelist',[])
+
 
   const handleItemDelete = (id) => {
     let copy = data.slice()
