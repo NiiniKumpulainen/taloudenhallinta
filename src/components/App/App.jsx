5df0becabf999a55da262546067388bc149338bd
Niini Kumpulainen <p22567@edu.sasky.fi>
2025-02-05 15:31:18 +0200

    muuttaa tietojen tallennuksen käyttäjäkohtaiseksi

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,41 +2,50 @@ import { useState } from 'react'
 import useLocalStorage from '../../shared/uselocalstorage/uselocalstorage'
 import AppRouter from '../AppRouter'
 import testdata from './testdata.js'
-import { addDoc, collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore'
-
-
 import firebase, { auth } from './firebase.js'
+import { addDoc, collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore'
 import { onAuthStateChanged } from 'firebase/auth'
-import Startup from '../Startup'
-
 import { useEffect } from 'react'
-
-
-
+import Startup from '../Startup'
 function App() {
   const [data, setData] = useState([])
-  
   const [typelist, setTypelist] = useState([])
   const [user, setUser] = useState()
-
-  
   const firestore = getFirestore(firebase)
 
-
-
+  useEffect( () => {
+    if (user) {
+      const unsubscribe = onSnapshot(query(collection(firestore,`user/${user.uid}/item`),
+                                           orderBy('paymentDate', 'desc')),
+                                     snapshot => {
+        const newData = []
+        snapshot.forEach( doc => {
+          newData.push({ ...doc.data(), id: doc.id })
+        })
+        setData(newData)
+      })
+      return unsubscribe
+    } else {
+      setData([])
+    }
+  }, [user])
 
   useEffect( () => {
-    const unsubscribe = onSnapshot(query(collection(firestore,'type'),
-                                         orderBy('type')),
-                                   snapshot => {
-      const newTypelist = []
-      snapshot.forEach( doc => {
-        newTypelist.push(doc.data().type)
+    if (user) {
+      const unsubscribe = onSnapshot(query(collection(firestore,`user/${user.uid}/type`),
+                                           orderBy('type')),
+                                     snapshot => {
+        const newTypelist = []
+        snapshot.forEach( doc => {
+          newTypelist.push(doc.data().type)
+        })
+        setTypelist(newTypelist)
       })
-      setTypelist(newTypelist)
-    })
-    return unsubscribe
-  }, []) 
+      return unsubscribe
+    } else {
+      setTypelist([])
+    }
+  }, [user])
 
   useEffect( () => {
     onAuthStateChanged(auth, user => {
@@ -44,24 +53,21 @@ function App() {
     })
   }, [])
 
-
-
   const handleItemDelete = async (id) => {
-    await deleteDoc(doc(firestore, 'item', id))
+    await deleteDoc(doc(firestore, `user/${user.uid}/item`, id))
   }
+
   const handleItemSubmit = async (newitem) => {
-    await setDoc(doc(firestore, 'item', newitem.id), newitem)
+    await setDoc(doc(firestore, `user/${user.uid}/item`, newitem.id), newitem)
   }
 
   const handleTypeSubmit = async (type) => {
-    await addDoc(collection(firestore,'type'),{type: type})
+    await addDoc(collection(firestore,`user/${user.uid}/type`),{type: type})
   }
 
-  
- 
-        return (
+  return (
     <>
-    { user ?
+      { user ?
           <AppRouter data={data}
                      typelist={typelist}
                      onItemSubmit={handleItemSubmit}
@@ -74,4 +80,4 @@ function App() {
     </>
   )
 }
-export default App
+export default App  
