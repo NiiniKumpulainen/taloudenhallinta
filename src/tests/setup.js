8c2e448cebb5df3980daa601aba5522a95024611
Niini Kumpulainen <p22567@edu.sasky.fi>
2025-02-06 09:24:28 +0200

    lisää Storybook-dokumentaation

--- src/tests/setup.js
+++
@@ -1,8 +0,0 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
-
-// Siistitään ympäristö jokaisen testitapauksen jälkeen.
-afterEach(() => {
-  cleanup()
-})
